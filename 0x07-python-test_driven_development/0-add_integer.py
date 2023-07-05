21b34dff39fc30930fd471b00a3d7fd72552c25d
HichamZineb <hichamelamine@hotmail.com>
2023-07-05 13:44:57 -0700

    update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+
 def add_integer(a, b=98):
+    """
+    Returns a + b
+
+    Floats are casted into ints before addition
+
+    Raises:
+        TypeError: if a or b are not integers or floats
+    """
+    
     if type(a) not in [int, float] or a is None:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float] or b is None:
