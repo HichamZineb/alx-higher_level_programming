3593dc82fc852ddef1c0cbce206eba1911642d0e
HichamZineb <hichamelamine@hotmail.com>
2023-07-11 08:41:37 -0700

    newfile 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""reads a text file"""
+
+def read_file(filename=""):
+    """
+    use the with statement
+    don’t need to manage exceptions.
+    """
+
+    with open(filename, encoding=utf-8) as file:
+        print(file.read())
