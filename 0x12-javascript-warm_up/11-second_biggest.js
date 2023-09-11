69fc83a7b8fc2ffe390e77129b9a632cca0601ef
HichamZineb <hichamelamine@hotmail.com>
2023-09-11 08:46:46 -0700

    update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,7 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+  const integers = process.argv.slice(2).map(arg => parseInt(arg));
+  integers.sort((a, b) => b - a);
+  console.log(integers[1]);
 }
