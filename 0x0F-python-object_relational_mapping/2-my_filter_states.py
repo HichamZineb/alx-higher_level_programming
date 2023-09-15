e896451f59b41a2f6e46f8853b4529cdfed68eea
HichamZineb <hichamelamine@hotmail.com>
2023-09-15 08:55:16 -0700

    newfile 2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" script that lists all states from the database hbtn_0e_0_usa """
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         password=argv[2], db=argv[3])
+
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT * "
+        "FROM states "
+        "WHERE name LIKE BINARY %s "
+        "ORDER BY id ASC",
+        (argv[4]),)
+    )
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
