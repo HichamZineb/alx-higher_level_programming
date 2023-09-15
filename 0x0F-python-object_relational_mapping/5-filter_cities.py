73899ff6011bd28dddd09baa725b2a97fe1dbc01
HichamZineb <hichamelamine@hotmail.com>
2023-09-15 10:00:17 -0700

    update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
         "INNER JOIN states ON cities.state_id = states.id "
         "WHERE states.name = %s "
         "ORDER BY cities.id ASC",
-        (arv[4],)
+        [arv[4]]
     )
 
     cities = cursor.fetchall()
