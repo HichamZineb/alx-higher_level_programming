4f032a5431eb68c6c50700d02a048f3d009e3fb8
HichamZineb <hichamelamine@hotmail.com>
2023-09-16 08:22:16 -0700

    update 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -18,9 +18,9 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    cities = session.query(State, City).join(City).order_by(City.id)
+    city = session.query(State, City).join(City).order_by(City.id)
 
-    for city, state_name in cities:
-        print("{}: ({}) {}".format(state_name, city.id, city.name))
+    for state, city in city:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
 
     session.close()
