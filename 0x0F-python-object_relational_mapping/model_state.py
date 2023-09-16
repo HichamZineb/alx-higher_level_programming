1bd7291602e13811113a2210671c68d6a30dcdc2
HichamZineb <hichamelamine@hotmail.com>
2023-09-16 02:50:35 -0700

    update model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,6 +11,8 @@ Base = declarative_base()
 
 
 class State(Base):
+    """ State class that inherits from Base """
+
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
